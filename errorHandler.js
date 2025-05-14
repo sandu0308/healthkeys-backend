22a60c11f5020bcdd51b30550eb6e0f7f559136f
sandu0308 <askar0008@gmail.com>
2025-05-14 11:19:53 +0500

    Delete errorHandler.js

--- errorHandler.js
+++
@@ -1,8 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({
-        success: false,
-        message: 'Ошибка сервера',
-        error: err.message
-    });
-};
